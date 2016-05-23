e4ea58abfdf17b3a07caf609e59cbb911fc5d725
Michal Drwiega <drwiega.michal@gmail.com>
2016-05-23 15:37:01 +0200

    First commit

---
+++ include/drive_controller/drive_controller_node.h
@@ -0,0 +1,154 @@
+/******************************************************************************
+ * Software License Agreement (BSD License)
+ *
+ * Copyright (c) 2016, Michal Drwiega (drwiega.michal@gmail.com)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     1. Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *     2. Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ *     3. Neither the name of the copyright holder nor the names of its
+ *        contributors may be used to endorse or promote products derived
+ *        from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *****************************************************************************/
+/**
+ * @file   drive_controller_node.h
+ * @author Michal Drwiega (drwiega.michal@gmail.com)
+ * @date   2016
+ * @brief  drive_controller package
+ */
+
+#ifndef drive_controller_NODE
+#define drive_controller_NODE
+
+#include <ros/ros.h>
+#include <dynamic_reconfigure/server.h>
+#include <std_msgs/UInt8.h>
+#include <std_msgs/UInt16.h>
+#include <std_msgs/Float64.h>
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf/tf.h>
+#include <boost/thread/mutex.hpp>
+
+#include <drive_controller/DriveControllerConfig.h>
+
+#include <drive_controller/motor_controller_api.h>
+
+//#define DEBUG_NODE
+
+
+namespace drive_controller
+{
+class DriveControllerNode
+{
+public:
+  DriveControllerNode(ros::NodeHandle& n, ros::NodeHandle& pnh);
+
+  ~DriveControllerNode();
+  /**
+   * @brief writeDataToDevice
+   */
+  void writeDataToDevice();
+  /**
+   * @brief readDataFromDevice
+   */
+  void readDataFromDevice();
+  /**
+   * @brief publishDataToTopics
+   */
+  void publishDataToTopics();
+private:
+  /**
+   * @brief velocitiesCb is callback which is called when new velocities command received
+   *
+   * @param cmd_vel Velocities commands
+   */
+  void velocitiesCb(const geometry_msgs::Twist cmd_vel);
+
+  void digitalOutputsCb(const std_msgs::UInt8 do_states);
+
+  /**
+   * @brief reconfigureCb is dynamic reconfigure callback
+   *
+   * Callback is necessary to set ROS parameters with dynamic reconfigure server.
+   *
+   * @param config Dynamic Reconfigure object.
+   * @param level Dynamic Reconfigure level.
+   */
+  void reconfigureCb(drive_controller::DriveControllerConfig& config, uint32_t level);
+
+private: // Private fields
+
+  ros::NodeHandle pnh_;                ///< Private node handler
+  // Publishers
+  ros::Publisher  pub_status_;         ///< Drive controller status publisher
+  ros::Publisher  pub_odom_;           ///< Odometry data publisher
+  ros::Publisher  pub_mot_voltage_;    ///< Motors voltage publisher
+  ros::Publisher  pub_digital_inputs_; ///< Digital inputs states publisher
+  tf2_ros::TransformBroadcaster tf_broadcaster_; ///< Broadcaster for tf publishing
+
+  // Subscribers
+  ros::Subscriber sub_cmd_vel_;         ///< Velocity commands subscriber
+  ros::Subscriber sub_digital_outputs_; ///< Subscriber for digital outputs ports
+
+  /// Dynamic reconfigure server
+  dynamic_reconfigure::Server<drive_controller::DriveControllerConfig> dyn_srv_;
+
+  MotorControllerAPI     mc_api_;    ///< HW controller API object
+  MotorControllerConfig  mc_config_; ///< HW controller configuration
+  MotorControllerDataSet mc_data_;   ///< Data received from HW module
+  ControlDataSet         mc_ctrl_;   ///< HW controller control data
+
+  //-----------------------------------------------------------------------------------------------
+  // ROS parameters setted when node starts
+  //-----------------------------------------------------------------------------------------------
+  float back_wheels_separation_;    ///< Back robot wheels separation in meters
+  float front_wheels_separation_;   ///< Front robot wheels separation in meters
+  std::string base_frame_id_;       ///< Frame for mobile robot base (child frame for odometry)
+  std::string odom_frame_id_;       ///< Frame for odometry
+
+  //-----------------------------------------------------------------------------------------------
+  // ROS parameters changed by dynamic reconfigure
+  //-----------------------------------------------------------------------------------------------
+  float cmd_vel_timeout_; ///< Velocity commands timeout in ms
+
+  // Velocities and acceleration limits
+  float linear_velocity_min_;      ///< Minimum linear velocity in m/s
+  float linear_velocity_max_;      ///< Maximum linear velocity in m/s
+  float angular_velocity_min_;     ///< Minimum angular velocity in rad/s
+  float angular_velocity_max_;     ///< Maximum angular velocity in rad/s
+  float linear_acceleration_min_;  ///< Minimum linear acceleration in m/s^2
+  float linear_acceleration_max_;  ///< Maximum linear acceleration in m/s^2
+  float angular_acceleration_min_; ///< Minimum angular acceleration in rad/s^2
+  float angular_acceleration_max_; ///< Maximum angular acceleration in rad/s^2
+  //-----------------------------------------------------------------------------------------------
+
+  // Simple watchdog which send zero speeds to microcontroller if no new cmd_vel
+  unsigned int watchdog_cnt_;
+  // time_out need to be calculated as max cnt value
+
+  ///< Prevents the connectCb and disconnectCb from being called until everything is initialized.
+  boost::mutex connection_mutex_;
+};
+}
+
+#endif
---
+++ src/drive_controller_node.cpp
@@ -0,0 +1,237 @@
+/******************************************************************************
+ * Software License Agreement (BSD License)
+ *
+ * Copyright (c) 2016, Michal Drwiega (drwiega.michal@gmail.com)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     1. Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *     2. Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ *     3. Neither the name of the copyright holder nor the names of its
+ *        contributors may be used to endorse or promote products derived
+ *        from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *****************************************************************************/
+/**
+ * @file   main.cpp
+ * @author Michal Drwiega (drwiega.michal@gmail.com)
+ * @date   2016
+ * @brief  drive_controller package
+ */
+
+#include <drive_controller/drive_controller_node.h>
+
+#include <iostream>
+
+using namespace drive_controller;
+
+
+//==============================================================================
+DriveControllerNode::DriveControllerNode(ros::NodeHandle& n, ros::NodeHandle& pnh):
+  pnh_(pnh), dyn_srv_(pnh),
+  base_frame_id_("wheel_base_link"),
+  odom_frame_id_("wheel_odom"),
+  watchdog_cnt_(0)
+{
+  boost::mutex::scoped_lock lock(connection_mutex_);
+  
+  // Set callback for dynamic reconfigure server
+  dyn_srv_.setCallback(boost::bind(&DriveControllerNode::reconfigureCb, this, _1, _2));
+  
+  // Velocity commands subscription (geometry_msgs/Twist)
+  sub_cmd_vel_ = n.subscribe("cmd_vel", 10,
+                             &DriveControllerNode::velocitiesCb, this);
+
+  // Velocity commands subscription (std_msgs/UInt8)
+  /*sub_cmd_vel_ = n.subscribe("drive_controller/digital_outputs", 10,
+                             &DriveControllerNode::digitalOutputsCb, this);*/
+
+  // Odometry data publishing (sensor_msgs/Odom)
+  pub_odom_ = n.advertise<nav_msgs::Odometry>("odom", 1);
+
+  // Digital inputs states publisher
+  pub_digital_inputs_ = n.advertise<std_msgs::UInt8>("digital_inputs", 1);
+
+  // Status publisher
+  pub_status_ = n.advertise<std_msgs::UInt16 >("drive_status", 1);
+
+  // Motors voltage publisher
+  pub_mot_voltage_ = n.advertise<std_msgs::Float64>("drive_controller/motors_voltage",1);
+
+  mc_ctrl_.ang_z_vel = 0.0;
+  mc_ctrl_.lin_x_vel = 0.0;
+
+  // Connect to HW device
+  if (mc_api_.connectToDevice(DEVICE_VID, DEVICE_PID))
+  {
+    ROS_INFO("Connected to drive controller device.");
+  }
+  else
+  {
+    ROS_ERROR("HW device connection can't be established.");
+  }
+
+  // Get constant ros params values from server params
+//  n.param<float>("back_wheels_separation", back_wheels_separation_, 1.0);
+//  n.param<float>("front_wheels_separation", front_wheels_separation_, 1.0);
+  base_frame_id_ = "base_link";
+  odom_frame_id_ = "odom";
+//  n.param<std::string>("base_frame_id", base_frame_id_, "base_link");
+//  n.param<std::string>("base_frame_id", odom_frame_id_, "odom");
+}
+
+//==============================================================================
+DriveControllerNode::~DriveControllerNode()
+{
+  if (mc_api_.isConnected())
+    mc_api_.disconnectDevice();
+
+  sub_cmd_vel_.shutdown();
+  sub_digital_outputs_.shutdown();
+  pub_status_.shutdown();
+  pub_mot_voltage_.shutdown();
+  pub_odom_.shutdown();
+  pub_digital_inputs_.shutdown();
+}
+
+//==============================================================================
+void DriveControllerNode::writeDataToDevice()
+{
+  // Check if watchdog maximum value exceeded
+  if (++watchdog_cnt_ > 50)
+  {
+    mc_ctrl_.setReferenceSpeed(BL, 0);
+    mc_ctrl_.setReferenceSpeed(BR, 0);
+    mc_ctrl_.setReferenceSpeed(FL, 0);
+    mc_ctrl_.setReferenceSpeed(FR, 0);
+
+    mc_ctrl_.lin_x_vel = 0;
+    mc_ctrl_.ang_z_vel = 0;
+  }
+
+ // mc_ctrl_.lin_x_vel = 0.2;
+  //mc_ctrl_.ang_z_vel = 0.0;
+
+  mc_api_.sendCtrlDataToDevice(this->mc_ctrl_);
+
+  std::cout << "\nSend data: lin_vel = " << mc_ctrl_.lin_x_vel << "  ang_vel = "
+            << mc_ctrl_.ang_z_vel << "  ";
+}
+
+//==============================================================================
+void DriveControllerNode::readDataFromDevice()
+{
+  mc_api_.getProcessDataFromDevice(mc_data_);
+
+  std::cout << "\nRead data: x = " << mc_data_.x_ << "  y = " << mc_data_.y_
+            << "  theta = " << mc_data_.theta_ << "  lin_vel = " << mc_data_.lin_vel_
+            << "  ang_vel = " << mc_data_.ang_vel_;
+}
+
+//==============================================================================
+void DriveControllerNode::publishDataToTopics()
+{
+  // Publish status
+  std_msgs::UInt16 status;
+  status.data = (uint16_t) mc_data_.state_reg0;
+  pub_status_.publish(status);
+
+  // Publish motors voltage
+  std_msgs::Float64 voltage;
+  voltage.data = mc_data_.getMotorsVoltage_mV() / 1000.0;
+  pub_mot_voltage_.publish(voltage);
+
+  // Publish odometry data to topic
+  nav_msgs::Odometry odom;
+  odom.header.stamp = ros::Time::now();
+  odom.header.frame_id = odom_frame_id_;
+  // Set the odom position and orientation
+
+  odom.pose.pose.position.x = mc_data_.x_;
+  odom.pose.pose.position.y = mc_data_.y_;
+  odom.pose.pose.position.z = 0;
+
+  geometry_msgs::Quaternion orient = tf::createQuaternionMsgFromYaw(mc_data_.theta_);
+  odom.pose.pose.orientation = orient;
+
+  // Set the velocities
+  odom.child_frame_id = base_frame_id_;
+  odom.twist.twist.linear.x = mc_data_.lin_vel_;
+  odom.twist.twist.linear.y = 0;
+  odom.twist.twist.linear.z = 0;
+  odom.twist.twist.angular.x = 0;
+  odom.twist.twist.angular.y = 0;
+  odom.twist.twist.angular.z = mc_data_.ang_vel_;
+
+  odom.pose.covariance[0] = 0.00001;
+  odom.pose.covariance[7] = 0.00001;
+  odom.pose.covariance[14] = 1000000000000.0;
+  odom.pose.covariance[21] = 1000000000000.0;
+  odom.pose.covariance[28] = 1000000000000.0;
+  odom.pose.covariance[35] = 0.001;
+
+  pub_odom_.publish(odom);
+
+  // Publish odometry transformations (tf)
+  geometry_msgs::TransformStamped transform;
+  transform.header.stamp = odom.header.stamp;
+  transform.header.frame_id = odom_frame_id_;
+  transform.child_frame_id = base_frame_id_;
+  transform.transform.translation.x = odom.pose.pose.position.x;
+  transform.transform.translation.y = odom.pose.pose.position.y;
+  transform.transform.translation.z = odom.pose.pose.position.z;
+  transform.transform.rotation = orient;
+  tf_broadcaster_.sendTransform(transform);
+}
+
+//==============================================================================
+void DriveControllerNode::velocitiesCb(const geometry_msgs::Twist cmd_vel)
+{
+  // Calculate references wheels speeds based on inverse kinematics
+  std::vector<float> ref_speeds(4);
+
+  mc_ctrl_.lin_x_vel = cmd_vel.linear.x;
+  mc_ctrl_.ang_z_vel = cmd_vel.angular.z;
+
+  // Set ref speeds in control structure
+  /*mc_ctrl_.setReferenceSpeed(BL, ref_speeds[BL]);
+  mc_ctrl_.setReferenceSpeed(BR, ref_speeds[BR]);
+  mc_ctrl_.setReferenceSpeed(FL, ref_speeds[FL]);
+  mc_ctrl_.setReferenceSpeed(FR, ref_speeds[FR]);*/
+
+  // Reset watchdog counter
+  watchdog_cnt_ = 0;
+}
+
+//==============================================================================
+void digitalOutputsCb(const std_msgs::UInt8 do_states)
+{
+}
+
+//==============================================================================
+void DriveControllerNode::reconfigureCb(
+    drive_controller::DriveControllerConfig& config, uint32_t level)
+{
+  /* node_rate_hz_ = (unsigned int) config.rate;
+
+  calib_.setGroundMaxPoints(config.ground_max_points);
+
+  calib_.setReconfParamsUpdated(true);*/
+}
+
+//==============================================================================
